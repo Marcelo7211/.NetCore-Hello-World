d5663def52a013cfd6fadefff3756fb76c932f19
unknown <lcapelotto@gmail.com>
2022-01-27 12:15:30 -0300

    hello world

---
+++ HelloWorld/HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public string HelloWorld()
+        {
+            return "Ola Mundo!";
+        }
+
+    }
+}
